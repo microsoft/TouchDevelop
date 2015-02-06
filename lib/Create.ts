4dfe7dbd88b2e26e8bf21d051f648d804e014489
Michal Moskal <michal.moskal@microsoft.com>
2015-02-05 19:08:08 -0800

    NEW Add `Create->Ref of`

--- lib/Create.ts
+++ lib/Create.ts
@@ -10,4 +10,20 @@ module TDev.RT.Create
     {
         return new Collection<T>(type_T)
     }
+
+    //? Creates a `Ref of T` (single-field object), initialized to the default value of `T`
+    //@ [result].writesMutable
+    export function Ref_of<T>(s:IStackFrame, type_T:any): Ref<T>
+    {
+        var r = new Ref<T>()
+
+        if (type_T == "string")
+            r._set(<any>"", s)
+        else if (type_T == "number")
+            r._set(<any>0, s)
+        else if (type_T == "boolean")
+            r._set(<any>false, s)
+
+        return r
+    }
 }
