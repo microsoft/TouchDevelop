4a37eb7635355c9390599a6c27a30d62557727a3
Peli de Halleux <jhalleux@microsoft.com>
2015-02-11 09:33:19 -0800

    dummy visual studio solution to edit typescript in VS

---
+++ vsrefs.ts
@@ -0,0 +1,12 @@
+///<reference path='ast/refs.ts'/>
+///<reference path='storage/refs.ts'/>
+///<reference path='lib/refs.ts'/>
+///<reference path='rt/refs.ts'/>
+///<reference path='libwab/refs.ts'/>
+///<reference path='libcordova/refs.ts'/>
+///<reference path='libnode/refs.ts'/>
+///<reference path='libwinrt/refs.ts'/>
+///<reference path='runner/refs.ts'/>
+///<reference path='officemix/refs.ts'/>
+///<reference path='mc/refs.ts'/>
+///<reference path='editor/refs.ts'/>
