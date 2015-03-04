3f09766fcaf9b9988186b1186ad739a9d7c9cca6
Peli de Halleux <jhalleux@microsoft.com>
2015-03-03 18:40:00 -0800

    adding back "physics game starter"  for david renton

--- tools/client.ts
+++ tools/client.ts
@@ -3568,6 +3568,10 @@ var sectMakers = lf("makers");
 var sectTouchDevelop = lf("touchdevelop");
 var sectOthers = lf("others");
 
+/*
+    editorMode: 1 = block, 2 = coder, 3 = expert
+*/
+
 var templates: ScriptTemplate[] = [{
     title: lf("blank"),
     id: 'blank',
@@ -3622,7 +3626,9 @@ var templates: ScriptTemplate[] = [{
     section: sectBeginners,
     scriptid: 'mdrw',
     editorMode: 1,
-}, /*{
+}, 
+
+/*{
     title: lf("blank boostrap app"),
     id: 'blankbootstrapapp',
     icon: 'ArrowLR',
@@ -3702,6 +3708,15 @@ var templates: ScriptTemplate[] = [{
     scriptid: 'zqbpa',
     editorMode: 2,
 }, {
+    title: lf("blank docs"),
+    id: 'blankdocs',
+    icon: 'Controller',
+    name: 'ADJ docs',
+    description: lf("An empty documentation page."),
+    section: sectTouchDevelop,
+    scriptid: 'krvn',
+    editorMode: 3,
+}, {
     title: lf("blank tutorial"),
     id: 'blanktutorial',
     icon: 'Controller',
@@ -3710,7 +3725,7 @@ var templates: ScriptTemplate[] = [{
     section: sectTouchDevelop,
     scriptid: 'yujva',
     editorMode: 3,
-}, , {
+}, {
     title: lf("blank script plugin"),
     id: 'blankscriptplugin',
     icon: 'Brush',
@@ -3728,8 +3743,16 @@ var templates: ScriptTemplate[] = [{
     section: sectOthers,
     scriptid: 'zbxb',
     editorMode: 3,
-}
-];
+}, {
+    title: lf("physics game starter"),
+    id: 'physicsgamestarter',
+    icon: 'Controller',
+    name: 'ADJ game',
+    description: lf("Boiler plate code to create a game."),
+    section: sectOthers,
+    scriptid: 'kkwd',
+    editorMode: 2,
+}];
 
 export function main()
 {
