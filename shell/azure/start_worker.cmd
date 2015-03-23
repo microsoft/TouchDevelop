33de16c74c5117cdf48ed6207b3401e27999c510
Michal Moskal <michal.moskal@microsoft.com>
2015-03-23 10:34:12 -0700

    azure fixes

--- shell/azure/start_worker.cmd
+++ shell/azure/start_worker.cmd
@@ -7,7 +7,4 @@ echo %DATE% %TIME% Entering start_worker.cmd
 "%programfiles(x86)%\nodejs\"node.exe server.js
 echo %DATE% %TIME% server.js runtime terminated with code %ERRORLEVEL%
 
-echo %DATE% %TIME% Killing any remaining node.exe processes...
-%THIS%\kill.exe node
-
 echo %DATE% %TIME% Exiting start_worker.cmd
