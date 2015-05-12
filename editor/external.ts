9519dbbf54d6c58973036dfe037643d6f4321e1c
Jonathan Protzenko <protz@microsoft.com>
2015-05-12 08:52:17 -0700

    Graduation.
    Properly pull the latest version of the device library -- the current code would
    only fetch version N+1, not the most recent one.

--- editor/external.ts
+++ editor/external.ts
@@ -48,7 +48,10 @@ module TDev {
 
   export module External {
     export var TheChannel: Channel = null;
-    var deviceScriptId : string;
+    // [initAsync] will find the latest version of this script by walking up the
+    // update chain, but in order to save on some API calls, this script id
+    // should be refreshed at regular intervals.
+    var deviceScriptId = "jtpmeh";
 
     import J = AST.Json;
 
@@ -64,14 +67,16 @@ module TDev {
       return errorMsg;
     }
 
-    export function initAsync(): Promise { // of nothing
-      return deviceScriptId ? Promise.as()
-          : Browser.TheApiCacheMgr.getAsync('dhhzzl', true)
+    export function pullLatestLibraryVersion(): Promise { // of nothing
+      return Browser.TheApiCacheMgr.getAsync(deviceScriptId, true)
           .then((script: JsonScript) => {
-            if (script)
+            if (deviceScriptId != script.updateid) {
+              // Found a new version, keep going
               deviceScriptId = script.updateid;
-            else
-                deviceScriptId = 'dhhzzl';
+              return pullLatestLibraryVersion();
+            } else {
+              return Promise.as();
+            }
           });
     }
 
@@ -112,7 +117,7 @@ module TDev {
       iframe: HTMLIFrameElement,
       guid: string): Promise // of Channel
     {
-      return initAsync().then(() => new Channel(editor, iframe, guid));
+      return pullLatestLibraryVersion().then(() => new Channel(editor, iframe, guid));
     }
 
     export class Channel {
