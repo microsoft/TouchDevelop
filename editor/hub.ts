8e7e18314b9719d3f9d4d9bf3cf56e98d2d32b9f
Peli de Halleux <jhalleux@microsoft.com>
2015-06-01 12:12:04 -0700

    adding publish to cas button

--- editor/hub.ts
+++ editor/hub.ts
@@ -356,6 +356,7 @@ module TDev.Browser {
                     socialNetworks: true,
                     socialNetworkvimeo:true,
                     publishAsHidden: true,
+                    publishToComputingAtSchools: true,
                 }
             },
         },
@@ -396,6 +397,7 @@ module TDev.Browser {
                     socialNetworks: true,
                     socialNetworkvimeo: true,
                     publishAsHidden: true,
+                    publishToComputingAtSchools: true,
 
                     // editor specific                  
                     publishDescription: true,
