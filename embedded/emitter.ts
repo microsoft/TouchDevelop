77e39ec5ea63ac134a4cbc6a8bea15252edb6a0d
Jonathan Protzenko <protz@microsoft.com>
2015-06-23 17:07:52 -0700

    C++ compiler.
    Don't compile commented out code.

--- embedded/emitter.ts
+++ embedded/emitter.ts
@@ -150,14 +150,19 @@ module TDev {
           elseBranch: J.JStmt[],
           isElseIf: boolean)
       {
-        return [
-          env.indent, isElseIf ? "else " : "", "if (" + this.visit(env, cond) + "){\n",
-          this.visitMany(indent(env), thenBranch) + "\n",
-          env.indent, "}",
-          elseBranch ? " else {\n" : "",
-          elseBranch ? this.visitMany(indent(env), elseBranch) + "\n" : "",
-          elseBranch ? env.indent + "}" : ""
-        ].join("");
+        // TouchDevelop abuses "if false" to comment out code. Commented out
+        // code is not type-checked, so don't try to compile it.
+        if (cond.tree.nodeType == "booleanLiteral" && (<J.JBooleanLiteral> cond.tree).value === false)
+          return "";
+        else
+          return [
+            env.indent, isElseIf ? "else " : "", "if (" + this.visit(env, cond) + "){\n",
+            this.visitMany(indent(env), thenBranch) + "\n",
+            env.indent, "}",
+            elseBranch ? " else {\n" : "",
+            elseBranch ? this.visitMany(indent(env), elseBranch) + "\n" : "",
+            elseBranch ? env.indent + "}" : ""
+          ].join("");
       }
 
       private resolveCall(env: EmitterEnv, receiver: J.JExpr, name: string) {
