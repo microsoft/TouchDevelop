8ba59fc2f104fb20564fb89e14c6ffd708d01cfb
Peli de Halleux <jhalleux@microsoft.com>
2015-10-02 22:43:09 +0100

    removing clickonce support, renaming uploader

--- microbit/loader/Microbit.Loader/Properties/Resources.Designer.cs
+++ microbit/loader/Microbit.Loader/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.MicroBit.Loader.Properties {
+namespace Microsoft.MicroBit.Uploader.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.MicroBit.Loader.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.MicroBit.Loader.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.MicroBit.Uploader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
