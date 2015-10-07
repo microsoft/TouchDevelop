2c9d8d30cd28e790ebbffedffbb0b5cd3e51c53f
Michal Moskal <michal.moskal@microsoft.com>
2015-10-06 18:25:10 -0700

    Always allow locals capture when bitvm=1

--- editor/default.ts
+++ editor/default.ts
@@ -226,7 +226,7 @@ module TDev
         if (/withTracing/.test(url)) withTracing = true;
         if (/enableUndo/.test(url)) TDev.Collab.enableUndo = true;
         if (/nohub/.test(url) || Cloud.isRestricted()) { TDev.noHub = true; TDev.hubHash = "list:installed-scripts"; }
-        if (/usegcc=1/.test(url)) Cloud.useEmbeddedGcc = true;
+        if (/(usegcc|bitvm)=1/.test(url)) Cloud.useEmbeddedGcc = true;
 
         //if (/endKeywords/.test(url)) Renderer.useEndKeywords = true;
         if (/lfDebug/.test(url)) Util.translationDebug = true;
