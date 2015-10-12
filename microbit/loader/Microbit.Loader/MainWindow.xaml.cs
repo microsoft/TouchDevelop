7272960ea297d350201c41d601f9ff2bef6bc1f6
Peli de Halleux <jhalleux@microsoft.com>
2015-10-12 09:23:51 -0700

    after upload, rename file to .uploaded.hex to retain previous .hex copy.

--- microbit/loader/Microbit.Loader/MainWindow.xaml.cs
+++ microbit/loader/Microbit.Loader/MainWindow.xaml.cs
@@ -95,12 +95,9 @@ namespace Microsoft.MicroBit
         static string getVolumeLabel(DriveInfo di)
         {
             try { return di.VolumeLabel; }
-            catch (IOException)
-            { }
-            catch (SecurityException)
-            { }
-            catch(UnauthorizedAccessException)
-            {}
+            catch (IOException) { }
+            catch (SecurityException) { }
+            catch(UnauthorizedAccessException) {}
             return "";
         }
 
@@ -109,12 +106,24 @@ namespace Microsoft.MicroBit
             this.handleFile(e.FullPath);
         }
 
+        void runIOSafe(Action a)
+        {
+            try
+            {
+                a();
+            }
+            catch (IOException) { }
+            catch (NotSupportedException) { }
+            catch (UnauthorizedAccessException) { }
+            catch (ArgumentException) { }
+        }
+
         void handleFile(string fullPath)
         {
             try
             {
-		// In case this is data-url download, at least Chrome will not rename file, but instead write to it
-	        // directly. This mean we may catch it in the act. Let's leave it some time to finish writing.
+	        	// In case this is data-url download, at least Chrome will not rename file, but instead write to it
+    	        // directly. This mean we may catch it in the act. Let's leave it some time to finish writing.
                 Thread.Sleep(500);
 
                 var info = new System.IO.FileInfo(fullPath);
@@ -131,7 +140,6 @@ namespace Microsoft.MicroBit
                 }
 
                 this.updateStatus("uploading " + info.Length + " bytes...");
-
                 var trg = System.IO.Path.Combine(drive.RootDirectory.FullName, "firmware.hex");
                 File.Copy(info.FullName, trg, true);
                 this.updateStatus("uploading done");
@@ -139,10 +147,14 @@ namespace Microsoft.MicroBit
                 var del = (bool)Dispatcher.Invoke((Func<Boolean>)(() => this.DeleteOnUpload));
                 if (del)
                 {
-                    File.Delete(info.FullName);
+                    var temp = System.IO.Path.ChangeExtension(info.FullName, ".uploaded.hex");
+                    runIOSafe(() =>
+                    {
+                        File.Copy(info.FullName, temp, true);
+                        File.Delete(info.FullName);
+                    });
                     this.updateStatus("uploading and cleaning done");
                 }
-
                 return;
             }
             catch (IOException) { }
