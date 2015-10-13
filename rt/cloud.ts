2cf3a1e78414bd3011629964e3b50d00d4f38d8b
Jonathan Protzenko <protz@microsoft.com>
2015-10-13 16:33:58 -0700

    Bump the glue layer.
    Partly fixes bbc/microbit-extras#1206 (the part about the queueing behavior).

--- rt/cloud.ts
+++ rt/cloud.ts
@@ -3,7 +3,7 @@ module TDev.Cloud {
 
     export var lite = false;
     export var litePermissions:StringMap<boolean> = {};
-    var microbitGitTag = "v7";
+    var microbitGitTag = "v8";
 
     export var useEmbeddedGcc = true;
     export var useNativeCompilation = false;
