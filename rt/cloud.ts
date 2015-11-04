5fc69f1abca4592197502e0c5eb829860b14f327
Jonathan Protzenko <protz@microsoft.com>
2015-11-04 14:34:26 -0500

    Bump glue layer.

--- rt/cloud.ts
+++ rt/cloud.ts
@@ -4,7 +4,7 @@ module TDev.Cloud {
     export var lite = false;
     export var fullTD = true;
     export var litePermissions:StringMap<boolean> = {};
-    var microbitGitTag = "v17";
+    var microbitGitTag = "v18";
 
     export var useNativeCompilation = false;
 
