microsoft/TouchDevelop

commit 6c46983f166b4ba749fa7e9a8b0b44d1c5e8d8c1

New abuse report management dialog

editor/scriptList.ts
@@ -1968,6 +1968,7 @@
 
         public loadDetails(s:BrowserPage, tab = ""):void
         {
+            AbuseReview.hide()
             TipManager.setTip(null);
             ModalDialog.dismissCurrent();
             this.searchBox.style.opacity = "1"
@@ -3264,7 +3265,12 @@
 
         public mkSmallBox():HTMLElement
         {
-            return this.mkBoxCore(false).withClick(() => { this.parentBrowser.loadDetails(this) });
+            var box = this.mkBoxCore(false)
+            return box.withClick(() => { 
+                if (!/ visited/.test(box.className))
+                    box.className += " visited";
+                this.parentBrowser.loadDetails(this) 
+            });
         }
 
         public mkSmallBoxNoClick():HTMLElement
@@ -3524,8 +3530,8 @@
                 var loading = div("sdLoadingMore", lf("loading more..."));
                 this.tabContent.appendChild(loading);
                 this.loadMoreElementsAnd(cont, (elts:JsonPublication[], cont:string) => {
-                    loading.className = "";
-                    loading.setChildren(elts.map((c) => this.tabBox(c)));
+                    loading.removeSelf();
+                    elts.forEach((c) => this.tabContent.appendChild(this.tabBox(c)));
                     if (!!cont) loading.appendChild(this.loadMoreBtn(cont));
                     else loading.appendChild(this.finalListElt())
                 });
@@ -4888,8 +4894,24 @@
         public tabBox(cc:JsonIdObject):HTMLElement
         {
             var c = <JsonAbuseReport>cc;
-            return div(null,
+            var confirmed = false;
+            var delBtn = 
+                HTML.mkAsyncButton(lf("delete"), () => {
+                    if (confirmed) {
+                        return AbuseReportInfo.deletePubAsync(c.publicationid)
+                            .then(v => { if (v) delBtn.removeSelf() })
+                    } else {
+                        delBtn.setChildren(lf("for sure?"))
+                        confirmed = true
+                        return Promise.as()
+                    }
+                })
+            var delDiv = div(null, delBtn)
+            return div("abusePair",
                 ScriptInfo.labeledBox("", this.browser().getAnyInfoByEtag(<any>c).mkSmallBox()),
+                div("abuseButtons", 
+                    HTML.mkAsyncButton(lf("ignore"), () => AbuseReportInfo.setAbuseStatusAsync(cc.id, "ignored")),
+                    delBtn),
                 ScriptInfo.labeledBox(lf("on"), this.browser().getReferencedPubInfo(c).mkSmallBox()))
         }
     }
@@ -5177,6 +5199,14 @@
             return null;
         }
 
+        public initOverlay()
+        {
+            this._reports.initElements();
+            this._reports.tabLoaded = true;
+            this._reports.initTab();
+            return this._reports.tabContent;
+        }
+
         public initTab()
         {
             this._reports.initElements();
@@ -5186,6 +5216,16 @@
         }
 
         public mkTabsCore():BrowserTab[] { return [this]; }
+
+        public refresh()
+        {
+            this.invalidateCaches();
+            this._reports.initTab();
+        }
+
+        private invalidateCaches() {
+            TheApiCacheMgr.invalidate("abusereports");
+        }
     }
 
     export class SubscriptionsTab
@@ -6777,7 +6817,7 @@
             var hdiv = div("sdInlineHd", span("sdInlineHdLabel", hd));
             var r = div("inlineBlock sdInlineContentContainer sdScriptBox sdBox-" + hd.replace(/ /g, "-"), hdiv, elt);
             return r.withClick(() => {
-               KeyboardMgr.triggerClick(<HTMLElement>r.lastChild);
+                KeyboardMgr.triggerClick(<HTMLElement>r.lastChild);
             });
         }
 
@@ -9440,7 +9480,13 @@
             if (big)
                 res.className += " sdBigHeader";
 
-            return this.withUpdate(res, (u:JsonAbuseReport) => {
+            var firstResolved = true
+
+            Util.setTimeout(1, () => this.withUpdate(res, (u:JsonAbuseReport) => {
+                var par = res
+                while (par && !/abusePair/.test(par.className))
+                    par = <HTMLElement>par.parentNode;
+                var resolved = true
                 if (u.resolution == "ignored") {
                     icon.setChildren(HTML.mkImg("svg:fa-check-square-o,white"))
                     icon.style.background = "#308919";
@@ -9449,12 +9495,33 @@
                     icon.style.background = "#308919";
                 } else {
                     icon.setChildren(HTML.mkImg("svg:fa-flag,white"))
+                    resolved = false
                     icon.style.background = "#e72a2a";
                 }
+                if (firstResolved) {
+                    res.setAttribute("data-resolved", resolved ? "yes" : "no");
+                    if (par)
+                        par.setAttribute("data-resolved", resolved ? "yes" : "no");
+                    firstResolved = false
+                }
                 textBlock.setChildren([ u.text ]);
-                author.setChildren(["-- ", u.username]);
-                addInfoInner.setChildren([Util.timeSince(u.time) + " on " + u.publicationname]);
-            });
+                var usersuff = ""
+                if (u.usernumreports)
+                    usersuff = " (" + u.usernumreports + ")"
+                author.setChildren(["-- ", u.username + usersuff]);
+                var pubsuff = ""
+                if (u.publicationnumabuses) {
+                    pubsuff = " (" + u.publicationnumabuses
+                    var diff = u.publicationusernumabuses - u.publicationnumabuses
+                    if (diff > 0) {
+                        pubsuff += " + " + diff + " on other"
+                    }
+                    pubsuff += ")"
+                }
+                addInfoInner.setChildren([Util.timeSince(u.time) + " on " + u.publicationname + pubsuff]);
+            }));
+
+            return res;
         }
 
         static box(c:JsonAbuseReport)
@@ -9479,13 +9546,20 @@
 
             })
 
+
             return r;
         }
 
         public mkSmallBox():HTMLElement
         {
-            return this.mkBoxCore(false).withClick(() =>
-                TheApiCacheMgr.getAsync(this.publicId, true).done(resp => AbuseReportInfo.abuseOrDelete(resp.publicationid, false, this.publicId)));
+            var box = this.mkBoxCore(false);
+            box.withClick(() => {
+                if (!/ visited/.test(box.className))
+                    box.className += " visited";
+                TheApiCacheMgr.getAsync(this.publicId, true)
+                    .done(resp => AbuseReportInfo.abuseOrDelete(resp.publicationid, false, this.publicId));
+            })
+            return box;
         }
 
         public initTab()
@@ -9500,6 +9574,27 @@
         public mkBigBox():HTMLElement { return null; }
 
         public mkTabsCore():BrowserTab[] { return [this]; }
+
+        static setAbuseStatusAsync(abuseid:string, status:string) 
+        {
+            return Cloud.postPrivateApiAsync(abuseid, { resolution: status })
+            .then(() => {
+                TheApiCacheMgr.refetch(abuseid)
+            })
+        }
+
+        static deletePubAsync(pubid:string)
+        {
+            return Cloud.deletePrivateApiAsync(pubid)
+            .then(() => {
+                TheApiCacheMgr.refetch(pubid)
+                HTML.showProgressNotification(lf("gone."))
+                return true;
+            }, e => {
+                Cloud.handlePostingError(e, lf("delete publication"))
+                return false;
+            });
+        }
 
         static abuseOrDelete(pubid:string, doubleConfirm = false, abuseid:string = "", onDeleted : () => void = undefined)
         {
@@ -9518,15 +9613,11 @@
             .then((resp:CanDeleteResponse) => {
                 var b = TheHost
                 var del = () => {
-                    HTML.showProgressNotification(lf("deleting..."));            
-                    Cloud.deletePrivateApiAsync(pubid)
-                    .done(() => {
-                        TheApiCacheMgr.refetch(pubid)
-                        HTML.showProgressNotification(lf("gone."))
-                        if (onDeleted) onDeleted();
-                    }, e => Cloud.handlePostingError(e, lf("delete '{0}'", resp.publicationname)));
+                    HTML.showProgressNotification(lf("deleting..."));
+                    AbuseReportInfo.deletePubAsync(pubid).done(v => {
+                        if (v && onDeleted) onDeleted();
+                    })
                 }
-
                 var godelete = () => {
                     ModalDialog.ask(lf("Are you sure you want to delete '{0}'? No undo.", resp.publicationname),
                                     lf("delete"),
@@ -9543,12 +9634,8 @@
                 }
                 var setstatus = (status:string) => {
                     m.dismiss()
-                    Cloud.postPrivateApiAsync(abuseid, { resolution: status })
-                    .then(() => {
-                        TheApiCacheMgr.refetch(abuseid)
-                        HTML.showProgressNotification(lf("resolution updated."))
-                    })
-                    .done()
+                    AbuseReportInfo.setAbuseStatusAsync(abuseid, status)
+                    .done(() => HTML.showProgressNotification(lf("resolution updated.")))
                 }
 
                 if (!abuseid && resp.publicationuserid == Cloud.getUserId()) {

ast/help.ts
@@ -266,6 +266,11 @@ module TDev {
         text:string; // report text
         resolution:string;
         publicationuserid:string;
+
+        // this are available to moderators only
+        usernumreports?:number;
+        publicationnumabuses?:number;
+        publicationusernumabuses?:number;
     }
 
     export interface JsonChannel extends JsonPublication

editor/abusereview.ts
@@ -1,0 +1,56 @@
+///<reference path='refs.ts'/>
+
+module TDev.AbuseReview
+{
+    var root:HTMLElement;
+    var scroll:HTMLElement;
+    var pg:Browser.AbuseReportsPage;
+
+    export function show()
+    {
+        if (!root) {
+            var resolved = 
+                HTML.mkButton(lf("resolved: shown"), () => {
+                    if (root.getAttribute("data-hideresolved") == "yes") {
+                        resolved.setChildren(lf("resolved: shown"))
+                        root.setAttribute("data-hideresolved", "no")
+                    } else {
+                        resolved.setChildren(lf("resolved: hidden"))
+                        root.setAttribute("data-hideresolved", "yes")
+                    }
+                })
+            root = div("abuseReview",
+                HTML.mkButton(lf("abuse reports"), () => {
+                    show();
+                }),
+                HTML.mkButton(lf("refresh"), () => {
+                    refresh();
+                }),
+                resolved,
+                HTML.mkButton(lf("dismiss"), () => {
+                    hide();
+                }),
+                scroll = div("scroll"))
+            elt("root").appendChild(root)
+            pg = new Browser.AbuseReportsPage(Browser.TheHost);
+            scroll.setChildren(pg.initOverlay())
+        }
+
+        ModalDialog.dismissCurrent()
+        root.className = "abuseReview";
+    }
+
+    function refresh()
+    {
+        pg.refresh();
+    }
+
+    export function hide()
+    {
+        if (!root) return;
+
+        root.className = "abuseReview abuseReviewHidden";
+    }
+
+}
+

editor/hub.ts
@@ -689,7 +689,8 @@ module TDev.Browser {
                     (Cloud.hasPermission("internal") ? HTML.mkButton(lf("my groups"), () => { Util.setHash("#list:mygroups") }) : null),
                     (Cloud.hasPermission("internal") ? HTML.mkButton(lf("create script"), () => { TemplateManager.createScript() }) : null),
                     (Cloud.hasPermission("root-ptr") ? HTML.mkButton(lf("import docs"), () => { Browser.TheHub.importDocs() }) : null),
-                    (Cloud.hasPermission("user-mgmt") ? HTML.mkButton(lf("abuse reports"), () => { Util.setHash("#list:installed-scripts:abusereports") }) : null),
+                    // (Cloud.hasPermission("user-mgmt") ? HTML.mkButton(lf("abuse reports"), () => { Util.setHash("#list:installed-scripts:abusereports") }) : null),
+                    (Cloud.hasPermission("user-mgmt") ? HTML.mkButton(lf("abuse review"), () => { AbuseReview.show() }) : null),
                     (Cloud.hasPermission("admin") ? HTML.mkButton(lf("API config"), () => { editApiConfig() }) : null),
                     (Cloud.hasPermission("admin") ? HTML.mkButton(lf("permission review"), () => { permissionReview() }) : null),
                     (Cloud.hasPermission("admin") ? HTML.mkButton(lf("mbedint"), () => { mbedintUpdate() }) : null),

editor/refs.ts
@@ -51,6 +51,7 @@
 ///<reference path='messages.ts'/>
 ///<reference path='embedded.ts'/>
 ///<reference path='literalEditor.ts'/>
+///<reference path='abusereview.ts'/>
 ///<reference path='../intellitrain/train.ts'/>
 ///<reference path='../intellitrain/astutil.ts'/>
 ///<reference path='../intellitrain/features.ts'/>

rt/cloud.ts
@@ -910,6 +910,10 @@ module TDev.Cloud {
                 ModalDialog.info(lf("access denied"), lf("Your account is not authorized to perform this action."));
                 return;
             }
+            else if (e.status == 404) {
+                ModalDialog.info(lf("publication not found"), lf("Maybe it has been removed?"));
+                return;
+            }
             else if (e.status == 400)
                 throw new Error(lf("Cloud precondition violated ({0})", e.errorMessage));
         }

www/editor.css
@@ -3008,7 +3008,7 @@ div.iconThumb {
   right: 0;
   width: auto;
   position: absolute;
-  top: -0.3em;
+  top: -0.2em;
   overflow: hidden;
   height: 3.3em;
 }
@@ -6143,4 +6143,72 @@ table.dashboard tr td[data-completed^="yes"] {
   font-family: monospace;
   white-space: pre;
   color: #333;
+}
+
+.abuseReview {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+  background: white;
+  z-index: 1;
+  font-size: 16px;
+  background: #ff9;
+}
+
+.abuseReview .scroll {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 2em;
+  font-size: 1.5em;
+  padding-left: 1em;
+  background: #fff;
+  overflow-y:scroll;
+  overflow-x:hidden;
+}
+
+.abuseReviewHidden {
+  font-size: 0.5em;
+  height: 2.7em;
+  width: 9em;
+  right: auto;
+  top: auto;
+  left: 0.2em;
+  bottom: 0.1em;
+}
+
+.abuseReview .sdBox- {
+  width: 30em;
+}
+
+.abuseReview .sdCommentBlock {
+  font-size: 1.2em;
+}
+
+.abuseReview .sdHeaderOuter {
+  max-width: 50em;
+}
+
+.abuseReview .sdScriptBox {
+  margin: 0;
+}
+
+.abuseReview .visited {
+  background: #ecf;
+}
+
+.abuseReview[data-hideresolved^="yes"] .abusePair[data-resolved^="yes"]
+{
+  display: none;
+}
+
+.abuseButtons {
+  display: inline-block;
+  width: 8em;
+  font-size: 0.7em;
+  margin-top: 1.2em;
 }

commit 1b824432caa6e03c649f655df3ac0d13e8a3f2a4

Allow loading more than two screens of content; fixes bug introduced in 6c46983f

editor/scriptList.ts
@@ -3577,8 +3577,8 @@
                 this.loadMoreElementsAnd(cont, (elts:JsonPublication[], cont:string) => {
                     loading.removeSelf();
                     elts.forEach((c) => this.tabContent.appendChild(this.tabBox(c)));
-                    if (!!cont) loading.appendChild(this.loadMoreBtn(cont));
-                    else loading.appendChild(this.finalListElt())
+                    if (!!cont) this.tabContent.appendChild(this.loadMoreBtn(cont));
+                    else this.tabContent.appendChild(this.finalListElt())
                 });
             });
             return btn;

tools/client.ts
@@ -3953,8 +3953,8 @@ function reindexone(store:string, cont = "")
     var totalReindexed = 0
 
     var path = "admin/reindex/" + store
-    if (store == "groupmembers")
-        path = "groups/reimportmembers";
+    if (/\//.test(store))
+        path = store
 
     var loop = (cont:string) =>
         tdevGet(k.liteUrl + "api/" + path + "?count=100" + cont + k.key, resp => {