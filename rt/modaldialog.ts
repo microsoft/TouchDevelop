8783c496d6e5c8497067faea6321ce508677a50d
Peli de Halleux <jhalleux@microsoft.com>
2015-11-19 13:03:54 -0800

    fixed scrolling modal dialog

--- rt/modaldialog.ts
+++ rt/modaldialog.ts
@@ -429,7 +429,7 @@ module TDev
         }
 
         public setScroll() {
-            this.dialog.style.maxHeight = 'calc(100% - 1em)';
+            this.dialog.style.maxHeight = (SizeMgr.windowHeight * 0.85) / SizeMgr.topFontSize + "em";
             Util.setupDragToScroll(this.dialog);
         }
 
