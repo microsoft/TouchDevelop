3fe890b93f54b3e1dc1019b5cd1cc8313a3540d2
Michal Moskal <michal.moskal@microsoft.com>
2015-12-14 23:45:36 -0800

    Bump runtime to v34 - fix build

--- rt/cloud.ts
+++ rt/cloud.ts
@@ -4,7 +4,7 @@ module TDev.Cloud {
     export var lite = true;
     export var fullTD = true;
     export var litePermissions:StringMap<boolean> = {};
-    export var microbitGitTag = "v33";
+    export var microbitGitTag = "v34";
 
     export var _migrate: () => void;
 
