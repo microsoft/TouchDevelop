1b69398aaf75b281dad889f75da4f2964231e164
Peli de Halleux <jhalleux@microsoft.com>
2016-01-12 17:15:41 -0800

    updated 'magnetic force' to microT

--- rt/cloud.ts
+++ rt/cloud.ts
@@ -4,7 +4,7 @@ module TDev.Cloud {
     export var lite = true;
     export var fullTD = true;
     export var litePermissions:StringMap<boolean> = {};
-    export var microbitGitTag = "v47";
+    export var microbitGitTag = "v49";
 
     export var _migrate: () => void;
 
