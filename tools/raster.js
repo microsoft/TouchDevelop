dfcff6e82469bab3857cf29fd7f1bebd8e0fe6a0
Peli de Halleux <jhalleux@microsoft.com>
2016-01-19 21:37:25 -0800

    updated phantomjs rastering to query size of blockly canvas before rendering

--- tools/raster.js
+++ tools/raster.js
@@ -1,16 +1,45 @@
+var url = 'http://localhost:4242/editor/local/blockly/render.html?id=hgmgxx';
 var page = require('webpage').create();
-page.open('http://localhost:4242/editor/local/blockly/render.html?id=hgmgxx', function () {
+var needsResize = true;
+
+function render() {
     var tryRender = function () {
         var ready = page.evaluate(function () {
             return !!document.getElementById('blocklyDiv').getAttribute('data-ready');
         });
         if (ready) {
-            page.render('blocks.png');
-            phantom.exit();
+            if (needsResize) {
+                needsResize = false;
+                console.log('resizing...')
+                var metrics = page.evaluate(function () {
+                    return Blockly.mainWorkspace.getMetrics();
+                });
+                console.log('metrics:' + JSON.stringify(metrics, null, 2));
+                page = require('webpage').create();
+                page.viewportSize = { 
+                    width: metrics.contentWidth + metrics.contentLeft,
+                    height: metrics.contentHeight + metrics.contentTop,
+                };
+                page.clipRect = {
+                    top: metrics.contentTop,
+                    left: metrics.contentLeft,
+                    width: metrics.contentWidth,
+                    height: metrics.contentHeight
+                }
+                console.log('size: ' + JSON.stringify(page.viewportSize))
+                page.open(url, render);
+            } else { 
+                console.log('rendering...')
+                page.render('build/blocks.png');
+                phantom.exit();                                
+            }
         } else {
-            console.log('page not ready...')
+            console.log('.')
             setTimeout(tryRender, 1000);
         }
     };
     tryRender();
-});
+}
+
+console.log('loading ' + url);
+page.open(url, function () { render(); });
