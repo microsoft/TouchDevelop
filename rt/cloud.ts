99e25dc18ac80b245a5efb41d35895588e9b9771
Peli de Halleux <jhalleux@microsoft.com>
2016-01-25 13:47:46 -0800

    targetting DAL 1.4.10

--- rt/cloud.ts
+++ rt/cloud.ts
@@ -277,7 +277,7 @@ module TDev.Cloud {
                 return origin + "/blockly/render.html?id=" + encodeURIComponent(id);
             }
         },
-        microbitGitTag : "v56"
+        microbitGitTag : "v57"
     }
 
     export function isArtUrl(url : string) : boolean {
